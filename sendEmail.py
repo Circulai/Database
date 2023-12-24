8718387fe47d6dc2add4db5fe0f1f3d8bcc27841
AiSmartVision <leander-arun.ziehm@stud.th-deg.de>
2023-12-24 19:10:46 +0100

    add email workflow

--- sendEmail.py
+++ sendEmail.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import os 
+import os
 
 
 def send_email(subject, body):
@@ -10,21 +10,33 @@ def send_email(subject, body):
     RECEIVER_MAIL = os.environ["RECEIVER_MAIL"]
 
     message = MIMEMultipart()
-    message['From'] = SENDER_MAIL
-    message['To'] = RECEIVER_MAIL
-    message['Subject'] = subject
-    message.attach(MIMEText(body, 'plain'))
+    message["From"] = SENDER_MAIL
+    message["To"] = RECEIVER_MAIL
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login(SENDER_MAIL, SENDER_PASSWORD)
 
         server.sendmail(SENDER_MAIL, RECEIVER_MAIL, message.as_string())
 
-    print('Email sent successfully!',"subject: "+subject,"body: "+body)
-    
+    print("Email sent successfully!", "subject: " + subject, "body: " + body)
+
+
 if __name__ == "__main__":
-    subject = 'Test Email'
-    body = 'This is a test email sent from Python.'
+    print("Running sendEmail.py as main...")
+    subject = "Test Email"
+    body = "This is a test email sent from Python."
     send_email(subject, body)
 
+    print("Starting to read or write to test log file...")
+    fileName = "testLog.txt"
+    if os.path.exists(fileName):
+        with open(fileName, "a") as f:
+            f.write("\nThis got appended!")
+    else:
+        with open(fileName, "w") as f:
+            f.write("This is the first line of the test log file.")
+
+        # print("File not found!")
